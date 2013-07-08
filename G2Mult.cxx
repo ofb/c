aaff6de5dadbdadc8db284ea05cbedcb80cf5545
Owen <Owen.Barrett@gmail.com>
2013-07-07 19:12:41 -0500

    changed second mpz_clear to p2big rather than p1big

--- G2Mult.cxx
+++ G2Mult.cxx
@@ -208,7 +208,7 @@ void fillV(const unsigned int lambdaLength,
     for (int i = 0; i < 6; ++i)
       mpz_clear(p1big[i]);
     for (int i = 0; i < 4; ++i)
-      mpz_clear(p1big[i]);
+      mpz_clear(p2big[i]);
   }
 
   // clean up
@@ -222,7 +222,7 @@ void fillV(const unsigned int lambdaLength,
 void print2DV(const unsigned int lambdaLength, unsigned long p, mpc_t V[]) {
   FILE *output;
   char output_filename[ 32 ];
-  sprintf (output_filename, "G2MultOut/a2mult%06lu", p);
+  sprintf (output_filename, "G2MultOut/g2mult%06lu", p);
   output = fopen ( output_filename, "w" );
 
   fprintf(output, "{{%lu},{", p);
