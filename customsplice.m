81bf791245b6c49ecd2f98b42db8100be54a91d3
Owen <Owen.Barrett@gmail.com>
2013-12-30 14:45:43 +0800

    fixed customsplice.m and added spliceJob

--- customsplice.m
+++ customsplice.m
@@ -7,8 +7,8 @@ length=Length[old];
 
 c1=Table[Insert[old,new[[i]][[2]][[1]],{i,2,3}],{i,1,length}][[1]];
 c1=Table[Insert[c1,new[[i]][[2]][[2]],{i,2,4}],{i,1,length}][[1]];
-
-mag=c1;
+l=c1;
+mag=l;
 
 For[n=1,n<=length,n++,
 mag[[n]][[2]] =Flatten[mag[[n]][[2]]]
